11991d5968b73ed178e55c75aa586bbc96fdb951
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-08 10:01:03 +0530

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+
+
+    nums.sort((a,b)=>a-b)
+   return false
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==nums[i+1]){
+    }
+           return true
+        }
+};
+[
