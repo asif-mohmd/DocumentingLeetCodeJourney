5fb4ccd318ad3cb334fb888cf95b5f39e350f89d
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-11 09:36:52 +0530

    Create file

---
+++ 2828-CheckifaStringIsanAcronymofWords/2828-CheckifaStringIsanAcronymofWords.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
+
+    for(let i=0;i<words.length;i++){
+         str += words[i].charAt(0)
+    }
+    return s == str
+    let str = ''
+};
+[
