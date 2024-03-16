eb7e6f4a9203f02f1ee7943664b213368c411511
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-16 13:56:55 +0530

    Create file

---
+++ 3065-MinimumOperationstoExceedThresholdValueI/3065-MinimumOperationstoExceedThresholdValueI.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+
+    for(let i=0;i<nums.length;i++){
+       if(nums[i]>=k){
+    }
+
+    nums.sort((a,b)=>a-b)
+        return i
+       }
+    
+};
+[
