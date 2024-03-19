72a23bcb4e1bb377092c57be14b3f8b1a345589e
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-19 09:42:12 +0530

    Create file

---
+++ 231-PowerofTwo/231-PowerofTwo.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+
+    if(n==1){
+        return 1
+    }
+
+    while(k<=n){
+        if(n===k){
+    }
+            return true
+        }
+    return false
+      k *= 2
+let k = 2
+};
+1
