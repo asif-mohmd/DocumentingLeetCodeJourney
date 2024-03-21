940d689702f4122afa2b728344dd1fafdef864b7
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-21 10:18:27 +0530

    Create file

---
+++ 2190-MostFrequentNumberFollowingKeyInanArray/2190-MostFrequentNumberFollowingKeyInanArray.js
@@ -0,0 +1,23 @@
+            let target = nums[i + 1];
+            let count = map[target] + 1 || 1;
+            map[target] = count;
+            if (count > maxCount) {
+                maxVal = target;
+            }
+        }
+    }
+        if (nums[i] == key) {
+    return maxVal;
+};
+    let maxCount = 0;
+    for (let i = 0; i < nums.length - 1; i++) {    
+                maxCount = count;
+var mostFrequent = function(nums, key) {
+    let map = {}
+    let maxVal;
+ * @return {number}
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} key
+[
