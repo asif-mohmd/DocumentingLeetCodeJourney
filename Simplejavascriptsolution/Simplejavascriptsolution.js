401023304bca58d6320db54c13fbe013ddbbf295
MUHAMMED ASIF <66274448+asif-mohmd@users.noreply.github.com>
2024-03-28 17:57:05 +0530

    Create file

---
+++ Simplejavascriptsolution/Simplejavascriptsolution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    var colNum=0;
+    {
+    return colNum;
+};
+    columnTitle=columnTitle.split('').reverse().join('').toString();
+    for(var i=0;i<columnTitle.length;i++)
+    }
+      colNum += (columnTitle.charCodeAt(i) - 64) * Math.pow(26, i);
+
+"
